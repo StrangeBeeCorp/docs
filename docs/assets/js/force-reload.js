afac4ca5350ac24166bcb275b806d670074b4985
AnneLaure1307 <anne-laure.civeyrac@strangebee.com>
2025-06-12 11:40:12 +0200

    Removed js code

--- docs/assets/js/force-reload.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const homeLinks = document.querySelectorAll('a[href="/"]');
-  homeLinks.forEach(link => {
-    link.addEventListener('click', e => {
-      e.preventDefault();
-      window.location.href = '/';
-    });
-  });
-});
